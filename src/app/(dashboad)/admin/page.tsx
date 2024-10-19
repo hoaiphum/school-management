dffe433526dab1c7c37ba6e081e57762faa1798b
hoaiphum <hoaiphumluong@gmail.com>
2024-10-20 01:57:04 +0700

    Design some list and Admin page

--- src/app/(dashboad)/admin/page.tsx
+++ src/app/(dashboad)/admin/page.tsx
@@ -1,5 +1,37 @@
+import AttendanceChart from '@/components/AttendanceChart';
+import CountChart from '@/components/CountChart';
+import UserCard from '@/components/UserCard';
+
 const AdminPage = () => {
-    return <div className="">AdminPage</div>;
+    return (
+        <div className="flex flex-col p-4 gap-4 md:flex-row">
+            {/* LEFT */}
+            <div className="w-full lg:w-2/3 flex flex-col gap-8">
+                {/* USER CARD */}
+                <div className="flex gap-4 justify-between flex-wrap">
+                    <UserCard type="student" />
+                    <UserCard type="teacher" />
+                    <UserCard type="parent" />
+                    <UserCard type="staff" />
+                </div>
+                {/* MIDDLE CHART */}
+                <div className="flex gap-4 flex-col lg:flex-row">
+                    {/* COUNT CHART */}
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart />
+                    </div>
+                    {/* ATTENDANCE CHART */}
+                    <div className="w-full lg:w-2/3 h-[450px]">
+                        <AttendanceChart />
+                    </div>
+                </div>
+                {/* BOTTOM CHART */}
+                <div className="w-full h-[500px]"></div>
+            </div>
+            {/* RIGHT */}
+            <div className="w-full lg:w-1/3">r</div>
+        </div>
+    );
 };
 
 export default AdminPage;
--- src/app/(dashboad)/list/students/page.tsx
+++ src/app/(dashboad)/list/students/page.tsx
@@ -1,19 +1,18 @@
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role, teachersData } from '@/lib/data';
+import { role, studentsData } from '@/lib/data';
 import Image from 'next/image';
 import Link from 'next/link';
 
-type Teacher = {
+type Student = {
     id: number;
-    teacherId: string;
+    studentId: string;
     name: string;
-    email?: string;
-    phone: string;
     photo: string;
-    subjects: string[];
-    classes: string[];
+    phone?: string;
+    grade: number;
+    class: string;
     address: string;
 };
 
@@ -23,18 +22,13 @@ const columns = [
         accessor: 'info',
     },
     {
-        header: 'Teacher ID',
-        accessor: 'teacherId',
+        header: 'Student ID',
+        accessor: 'studentId',
         className: 'hidden md:table-cell',
     },
     {
-        header: 'Subjects',
-        accessor: 'subjects',
-        className: 'hidden md:table-cell',
-    },
-    {
-        header: 'Classes',
-        accessor: 'classes',
+        header: 'Grade',
+        accessor: 'grade',
         className: 'hidden md:table-cell',
     },
     {
@@ -53,8 +47,8 @@ const columns = [
     },
 ];
 
-const TeacherListPage = () => {
-    const renderRow = (item: Teacher) => (
+const StudentListPage = () => {
+    const renderRow = (item: Student) => (
         <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purple-100">
             <td className="flex items-center gap-4 p-4">
                 <Image
@@ -66,12 +60,11 @@ const TeacherListPage = () => {
                 />
                 <div className="flex flex-col">
                     <h3 className="font-semibold">{item.name}</h3>
-                    <p className="text-xs text-gray-500">{item?.email}</p>
+                    <p className="text-xs text-gray-500">{item.class}</p>
                 </div>
             </td>
-            <td className="hidden md:table-cell">{item.teacherId}</td>
-            <td className="hidden md:table-cell">{item.subjects.join(', ')}</td>
-            <td className="hidden md:table-cell">{item.classes.join(', ')}</td>
+            <td className="hidden md:table-cell">{item.studentId}</td>
+            <td className="hidden md:table-cell">{item.grade}</td>
             <td className="hidden md:table-cell">{item.phone}</td>
             <td className="hidden md:table-cell">{item.address}</td>
             <td>
@@ -95,7 +88,7 @@ const TeacherListPage = () => {
         <div className="bg-white p-4 m-4 mt-0 rounded-md flex-1">
             {/* TOP */}
             <div className="flex items-center justify-between">
-                <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+                <h1 className="hidden md:block text-lg font-semibold">All Students</h1>
                 <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
                     <TableSearch />
                     <div className="flex items-center gap-4 self-end">
@@ -114,13 +107,11 @@ const TeacherListPage = () => {
                 </div>
             </div>
             {/* LIST */}
-            <Table columns={columns} renderRow={renderRow} data={teachersData} />
+            <Table columns={columns} renderRow={renderRow} data={studentsData} />
             {/* PAGINATION */}
-            <div className="">
-                <Pagination />
-            </div>
+            <Pagination />
         </div>
     );
 };
 
-export default TeacherListPage;
+export default StudentListPage;
