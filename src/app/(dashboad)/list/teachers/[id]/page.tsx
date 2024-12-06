6694edbbf60a953fe701d6d7d819711d860419d0
hoaiphum <hoaiphumluong@gmail.com>
2024-12-07 02:25:45 +0700

    Complete tearcher single page design

--- src/app/(dashboad)/list/teachers/[id]/page.tsx
+++ src/app/(dashboad)/list/teachers/[id]/page.tsx
@@ -1,4 +1,8 @@
+import Announcements from '@/components/Announcements';
+import BigCalendar from '@/components/BigCalendar';
+import Performance from '@/components/Performance';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const SingleTeacherPage = () => {
     return (
@@ -8,7 +12,7 @@ const SingleTeacherPage = () => {
                 {/* TOP */}
                 <div className="flex flex-col lg:flex-row gap-4">
                     {/* USER INFO CARD */}
-                    <div className="bg-blue-200 py-6 px-4 rounded-md flex-1 flex gap-4">
+                    <div className="bg-schoolSky py-6 px-4 rounded-md flex-1 flex gap-4">
                         <div className="w-1/3">
                             <Image
                                 src="https://images.pexels.com/photos/2888150/pexels-photo-2888150.jpeg?auto=compress&cs=tinysrgb&w=1200"
@@ -82,10 +86,34 @@ const SingleTeacherPage = () => {
                 {/* BOTTOM */}
                 <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
                     <h1>Teacher&apos;s Schedule</h1>
+                    <BigCalendar />
                 </div>
             </div>
             {/* RIGHT */}
-            <div className="w-full xl:w-1/3">r</div>
+            <div className="w-full xl:w-1/3 flex flex-col gap-4">
+                <div className="bg-white p-4 rounded-md">
+                    <h1 className="text-xl font-semibold">Shortcuts</h1>
+                    <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+                        <Link className="p-3 rounded-md bg-schoolSkyLight" href="/">
+                            Teacher&apos;s Classes
+                        </Link>
+                        <Link className="p-3 rounded-md bg-schoolPurpleLight" href="/">
+                            Teacher&apos;s Students
+                        </Link>
+                        <Link className="p-3 rounded-md bg-schoolYellowLight" href="/">
+                            Teacher&apos;s Lessons
+                        </Link>
+                        <Link className="p-3 rounded-md bg-pink-50" href="/">
+                            Teacher&apos;s Exams
+                        </Link>
+                        <Link className="p-3 rounded-md bg-schoolSkyLight" href="/">
+                            Teacher&apos;s Assignments
+                        </Link>
+                    </div>
+                </div>
+                <Performance />
+                <Announcements />
+            </div>
         </div>
     );
 };
