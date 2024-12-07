ae19fe1b301e98972220c037582ceaa9d3b931d5
hoaiphum <hoaiphumluong@gmail.com>
2024-12-08 00:31:06 +0700

    Login page and protect routes with Clerk user role

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-const UserCard = ({ type }: { type: string }) => {
+const UserCard = async ({ type }: { type: 'admin' | 'teacher' | 'student' | 'parent' }) => {
     return (
         <div className="rounded-2xl odd:bg-schoolPurple even:bg-schoolYellow p-4 flex-1 min-w-[130px]">
             <div className="flex justify-between items-center">
