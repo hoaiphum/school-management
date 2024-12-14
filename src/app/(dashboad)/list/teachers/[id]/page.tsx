1e2d83b2605dd366f838885734c577ec033e8cdb
KimNgoc <kimngoc.68kg@gmail.com>
2024-12-14 14:52:10 +0700

    Fetch data from DB to Class, Lesson, Subject, Teacher

--- src/app/(dashboad)/list/teachers/[id]/page.tsx
+++ src/app/(dashboad)/list/teachers/[id]/page.tsx
@@ -115,7 +115,10 @@ const SingleTeacherPage = () => {
                 <div className="bg-white p-4 rounded-md">
                     <h1 className="text-xl font-semibold">Shortcuts</h1>
                     <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-                        <Link className="p-3 rounded-md bg-schoolSkyLight" href="/">
+                        <Link
+                            className="p-3 rounded-md bg-schoolSkyLight"
+                            href={`/list/classes?supervisorId=${'teacher'}`}
+                        >
                             Teacher&apos;s Classes
                         </Link>
                         <Link
@@ -124,7 +127,10 @@ const SingleTeacherPage = () => {
                         >
                             Teacher&apos;s Students
                         </Link>
-                        <Link className="p-3 rounded-md bg-schoolYellowLight" href="/">
+                        <Link
+                            className="p-3 rounded-md bg-schoolYellowLight"
+                            href={`/list/lesson?teacherId=${'teacher'}`}
+                        >
                             Teacher&apos;s Lessons
                         </Link>
                         <Link className="p-3 rounded-md bg-pink-50" href="/">
