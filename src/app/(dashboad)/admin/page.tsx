5ebb29d3f1cd28f306e4f66dd7e871ab451b1cda
hoaiphum <hoaiphumluong@gmail.com>
2025-01-20 22:56:59 +0700

    add new commit

--- src/app/(dashboad)/admin/page.tsx
+++ src/app/(dashboad)/admin/page.tsx
@@ -1,11 +1,13 @@
 import Announcements from '@/components/Announcements';
 import AttendanceChart from '@/components/AttendanceChart';
-import CountChart from '@/components/CountChart';
 import EventCalendar from '@/components/EventCalendar';
 import FinanceChart from '@/components/FinanceChart';
+import AttendanceChartContainer from '@/components/AttendanceChartContainer';
+import CountChartContainer from '@/components/CountChartContainer';
+import EventCalendarContainer from '@/components/EventCalendarContainer';
 import UserCard from '@/components/UserCard';
 
-const AdminPage = () => {
+const AdminPage = ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
     return (
         <div className="flex flex-col p-4 gap-4 md:flex-row">
             {/* LEFT */}
@@ -21,11 +23,11 @@ const AdminPage = () => {
                 <div className="flex gap-4 flex-col lg:flex-row">
                     {/* COUNT CHART */}
                     <div className="w-full lg:w-1/3 h-[450px]">
-                        <CountChart />
+                        <CountChartContainer />
                     </div>
                     {/* ATTENDANCE CHART */}
                     <div className="w-full lg:w-2/3 h-[450px]">
-                        <AttendanceChart />
+                        <AttendanceChartContainer />
                     </div>
                 </div>
                 {/* BOTTOM CHART */}
@@ -36,7 +38,7 @@ const AdminPage = () => {
             {/* RIGHT */}
             <div className="w-full lg:w-1/3 flex flex-col gap-8">
                 {/* EVENT CALENDAR */}
-                <EventCalendar />
+                <EventCalendarContainer searchParams={searchParams} />
                 <Announcements />
             </div>
         </div>
--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 
 const localizer = momentLocalizer(moment);
 
-const BigCalendar = () => {
+const BigCalendar = ({ data }: { data: { title: string; start: Date; end: Date }[] }) => {
     const [view, setView] = useState<View>(Views.WORK_WEEK);
     const handleOnChangeView = (selectedView: View) => {
         setView(selectedView);
@@ -15,7 +15,7 @@ const BigCalendar = () => {
     return (
         <Calendar
             localizer={localizer}
-            events={calendarEvents}
+            events={data}
             startAccessor="start"
             endAccessor="end"
             views={['work_week', 'day']}
